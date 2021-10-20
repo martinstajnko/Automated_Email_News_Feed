6a3e45a62d0ee34831a80b6149c1635a07de7930
martinstajnko <stajnko.martin93@gmail.com>
2021-10-20 09:19:24 +0200

    Code refactory, final version of the program

--- main.py
+++ main.py
@@ -6,49 +6,48 @@ import datetime
 from time import sleep
 
 
+def get_sender_credentials():
+    email = os.environ.get('EMAIL')
+    psw = os.environ.get('PSW')
+    return email, psw
+
+
+def get_receivers():
+    receivers = os.environ.get('RECEIVERS')
+    receivers = receivers.split(', ')
+    return receivers
+
+
+def get_interests():
+    interests = os.environ.get('INTERESTS')
+    interests = interests.split(', ')
+    return interests
+
+
+def send_news_feed_email(receivers, interests):
+    for (receiver, interest) in zip(receivers, interests):
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        news_feed = NewsFeed(interest=interest, 
+                            from_date=yesterday, 
+                            to_date=today, 
+                            language='en') 
+        email.send(to=receiver, 
+                    subject=f'NEWS ABOUT {interest}', 
+                    contents=f'Hi, this is me news bot!\n I have recent news about {interest} for you!\n{news_feed.get()}\n\n Sincerely, \n News Bot')
+                    #print(f'NEWS ABOUT {interests[0]}')
+                    #print(f'Hi, this is me news bot!\n I have recent news about {interests[0]} for you!\n{news_feed.get()}\n News Bot')
+
+
 while True:
 
-    if datetime.datetime.now().hour == 8 and datetime.datetime.now().minute == 00:
-        print('Executing!')    
-
+    if datetime.datetime.now().hour == 8 and datetime.datetime.now().minute == 40:
 
         load_dotenv('credentials.env')
-
-
-        def get_sender_credentials():
-            email = os.environ.get('EMAIL')
-            psw = os.environ.get('PSW')
-            return email, psw
-
-        def get_receivers():
-            receivers = os.environ.get('RECEIVERS')
-            receivers = receivers.split(', ')
-            return receivers
-
-        def get_interests():
-            interests = os.environ.get('INTERESTS')
-            interests = interests.split(', ')
-            return interests
-
-        def send_news_feed():
-            email, psw = get_sender_credentials()    
-            email = yagmail.SMTP(user=email, password=psw)
-            receivers = get_receivers()
-            interests = get_interests()
-            for (receiver, interest) in zip(receivers, interests):
-                today = datetime.datetime.now().strftime('%Y-%m-%d')
-                yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-                news_feed = NewsFeed(interest=interest, 
-                                    from_date=yesterday, 
-                                    to_date=today, 
-                                    language='en') 
-                email.send(to=receiver, 
-                            subject=f'NEWS ABOUT {interest}', 
-                            contents=f'Hi, this is me news bot!\n I have recent news about {interest} for you!\n{news_feed.get()}\n\n Sincerely, \n News Bot')
-
-                #print(f'NEWS ABOUT {interests[0]}')
-                #print(f'Hi, this is me news bot!\n I have recent news about {interests[0]} for you!\n{news_feed.get()}\n News Bot')
-        
-        send_news_feed()
+        email, psw = get_sender_credentials()    
+        email = yagmail.SMTP(user=email, password=psw)
+        receivers = get_receivers()
+        interests = get_interests()        
+        send_news_feed_email(receivers, interests)
 
     sleep(60)
--- news.py
+++ news.py
@@ -28,27 +28,32 @@ class NewsFeed:
 
 
     def get(self):
-
-        url = f'{self.base_url}' \
-              f'qInTitle={self.interest}&'\
-              f'from={self.from_date}&'\
-              f'to={self.to_date}&'\
-              f'language={self.language}&'\
-              f'apiKey={self.api_key}'
-
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
-        #print(articles)
-        #print('-' * 10)
+        url = self._build_url()
+        articles = self._get_articles(url)
         y = 0
         email_body = ''
         for article in articles:
             y = int(y)
             y = y + 1
             email_body = email_body + str(y) + '\n' + article['title'] + '\n' + article['url'] + '\n' + '\n'
-
         return email_body
+
+
+    def _get_articles(self, url):
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+        return articles
+
+
+    def _build_url(self):
+        url = f'{self.base_url}' \
+              f'qInTitle={self.interest}&'\
+              f'from={self.from_date}&'\
+              f'to={self.to_date}&'\
+              f'language={self.language}&'\
+              f'apiKey={self.api_key}'             
+        return url
 
 
 if __name__ == "__main__":
